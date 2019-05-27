630a985f59d26def526eff3864fd5f7d3f3f970f
Robin Lovelace <rob00x@gmail.com>
2019-05-27 12:25:33 +0100

    Add maxZoom to photomap()

--- R/photomap.R
+++ R/photomap.R
@@ -3,12 +3,14 @@
 #' @param iconWidth Width of the photo icon, in pixels
 #' @param iconHeight Height of the photo icon, in pixels
 #' @param icons File paths to icons of photos
+#' @param maxZoom The maximum zoom level allowed in the photomap
 #' @inheritParams photo_iconize
 #' @export
 #' @examples 
 #' f = geotagged_photo_paths()
 #' photomap(f)
-photomap = function(f, icons = NULL, dir_out = tempdir(), iconWidth = 100, iconHeight = 75) {
+#' photomap(f, maxZoom = 8)
+photomap = function(f, icons = NULL, dir_out = tempdir(), iconWidth = 100, iconHeight = 75, maxZoom = 18) {
   photos_geo = photos_sf(f)
   geometry = paste0(iconWidth, "x", iconHeight)
   if(is.null(icons)) {
@@ -16,7 +18,7 @@ photomap = function(f, icons = NULL, dir_out = tempdir(), iconWidth = 100, iconH
   }
   
   photo_icons = leaflet::makeIcon(iconUrl = f_icon, iconWidth = iconWidth, iconHeight = iconHeight,   iconAnchorX = 22, iconAnchorY = 94)
-  leaflet::leaflet(data = photos_geo) %>%
+  leaflet::leaflet(data = photos_geo, options = leaflet::leafletOptions(maxZoom = maxZoom)) %>%
     leaflet::addTiles() %>%
-    leaflet::addMarkers(icon = photo_icons, popup = )
+    leaflet::addMarkers(icon = photo_icons) 
 }
